9a5b1028458a935864ec6f24084f287f04a54c9d
Genadi Samokovarov <gsamokovarov@gmail.com>
2012-05-09 00:18:38 +0300

    Prepared for PYPI.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+version = '0.1.0'
+
+setup(
+    name='frames',
+    version=version,
+    description='Python stack frames goodies.',
+    author='Genadi Samokovarov',
+    author_email='gsamokovarov@gmail.com',
+    url='https://github.com/gsamokovarov/frames.py',
+    download_url='https://github.com/gsamokovarov/frames.py/tarball/%s' % version,
+    license='License :: OSI Approved :: MIT License',
+)
